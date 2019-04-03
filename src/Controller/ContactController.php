bbfb0013afb4411fa0e1b35ecf5326b8aed18529
dcorroyer <dylan.corroyer@wanadoo.fr>
2019-04-03 14:42:41 +0200

    fifth commit - contact/description

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+
+    /**
+     * @Route("/contact", name="contact.index")
+     * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @return Response
+     */
+    public function index(Request $request, Swift_Mailer $mailer): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $message = new Swift_Message();
+            $message
+                ->setTo('dylan.corroyer@wanadoo.fr')
+                ->setFrom('contact.dcorroyer@gmail.com')
+                ->setSubject('DCORROYER: ' . $contact->getSubject())
+                ->setBody($bodyHtml = $this->renderView('emails/contact.html.twig', [
+                    'contact' => $contact,
+                ]), 'text/html');
+
+            $mailer->send($message);
+            $this->addFlash('success', 'Votre Email a bien été envoyé !');
+            return $this->redirectToRoute('home');
+
+        }
+
+        return $this->render('pages/contact.html.twig', [
+            'form'          => $form->createView(),
+            'current_menu'  => 'contact'
+        ]);
+    }
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact
+{
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", max="100")
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", max="100")
+     */
+    private $subject;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min="10")
+     */
+    private $message;
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param null|string $name
+     * @return Contact
+     */
+    public function setName(?string $name): Contact
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param null|string $subject
+     * @return Contact
+     */
+    public function setSubject(?string $subject): Contact
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param null|string $email
+     * @return Contact
+     */
+    public function setEmail(?string $email): Contact
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param null|string $message
+     * @return Contact
+     */
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+}
