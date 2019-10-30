81385639ba1a875e899694a8f9ae920e5e838e3e
vagrant <vagrant@homestead>
2019-10-30 17:50:45 +0000

    homepage + admin homepage done

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-"# dcorroyer" 
+### Project acidarts.fr
 
-A Symfony 4 application to customer to be able to stand alone their portfolio by adding projets theirself.
+A Symfony 4 application to a 3D artist.
 
 Composed of:
 
-<ul>
-  <li>Project managing</li>
-  <li>Projects page</li>
-  <li>Description page</li>
-  <li>Contact form</li>
-</ul>
+- Project managing
+- Project page
+- Description page
+- Contact form
+
+BACK TODO:
+
+    Make description page to be manageable
+
+FRONT TODO/DONE:
+
+    TODO:
+    
+        homepage admin (responsive)
+        a project page
+        description
+        contact
+        admin new project
+        admin edit project
+    
+    DONE:
+    
+        homepage
+        homepage admin
+        login
+    
--- src/Migrations/Version20191028090825.php
+++ src/Migrations/Version20191028090825.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191023082048 extends AbstractMigration
+final class Version20191028090825 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,10 @@ final class Version20191023082048 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(191) NOT NULL, password VARCHAR(191) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, position INT NOT NULL, title VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, filename VARCHAR(255) DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, project_id INT DEFAULT NULL, videoname VARCHAR(191) NOT NULL, INDEX IDX_7CC7DA2C166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(191) NOT NULL, password VARCHAR(191) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, project_id INT NOT NULL, filename VARCHAR(191) NOT NULL, INDEX IDX_16DB4F89166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, position INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, filename VARCHAR(255) DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2C166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
     }
@@ -37,9 +37,9 @@ final class Version20191023082048 extends AbstractMigration
 
         $this->addSql('ALTER TABLE video DROP FOREIGN KEY FK_7CC7DA2C166D1F9C');
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89166D1F9C');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('DROP TABLE video');
-        $this->addSql('DROP TABLE picture');
         $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE video');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE picture');
     }
 }
