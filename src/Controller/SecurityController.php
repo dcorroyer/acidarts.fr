1e5870c0ebfe2cc7f840b9833f6c20c3f5771913
dcorroyer <dylan.corroyer@wanadoo.fr>
2021-03-12 17:26:01 +0100

    security and fixtures

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     * @param  AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error        = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig',[
+            'last_username' => $lastUsername,
+            'error'         => $error
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     * @throws Exception
+     */
+    public function logoutAction()
+    {
+        throw new Exception('Il y a un problème.');
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $hash = $this->encoder->encodePassword($user, 'password');
+
+        $user->setUsername('admin')
+            ->setEmail('dylan.corroyer@wanadoo.fr')
+            ->setPassword($hash);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
