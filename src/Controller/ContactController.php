1ff70e59e184f7705fc6856faa22f818dc17e2e1
dcorroyer <dylan.corroyer@wanadoo.fr>
2021-07-10 20:02:01 +0200

    contact form and page with css

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact_index")
+     * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @return Response
+     */
+    public function index(Request $request, Swift_Mailer $mailer): Response
+    {
+        $contact = new Contact();
+        $form    = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = new Swift_Message();
+            $message
+                ->setTo('dylan.corroyer@wanadoo.fr')
+                ->setFrom('contact.dcorroyer@gmail.com')
+                ->setSubject('Acidarts: ' . $contact->getSubject())
+                ->setBody($bodyHtml = $this->renderView('contact/email.html.twig', [
+                    'contact' => $contact,
+                ]), 'text/html')
+            ;
+
+            $mailer->send($message);
+
+            return $this->redirectToRoute('contact_index');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
