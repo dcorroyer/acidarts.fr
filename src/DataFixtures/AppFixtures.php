35ba2accf1fe982b86b3a182d7fe08a8bf9a46ff
dcorroyer <dylan.corroyer@wanadoo.fr>
2022-01-13 23:18:58 +0100

    upgrade and update to lts

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,33 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    /**
-     * @var UserPasswordEncoderInterface
-     */
-    private $encoder;
-
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function load(ObjectManager $manager): void
     {
-        $this->encoder = $encoder;
-    }
+        // $product = new Product();
+        // $manager->persist($product);
 
-    public function load(ObjectManager $manager)
-    {
-        $user = new User();
-        $hash = $this->encoder->encodePassword($user, 'password');
-
-        $user->setUsername('admin')
-            ->setEmail('dylan.corroyer@wanadoo.fr')
-            ->setPassword($hash);
-
-        $manager->persist($user);
         $manager->flush();
     }
 }
